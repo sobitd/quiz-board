ece1e9596c4f4f1d0721094eddb71f51fb24d4a3
sobitd <sobitdaniel@gmail.com>
2022-05-15 16:37:03 -0400

    Add variables that will help in DOM manipulation

---
+++ Assets/index.js
@@ -0,0 +1,9 @@
+//Declaring variables for DOM manipulation
+const userAnswers = document.getElementById("userForm")
+const question =  document.getElementById("question")
+const choices = document.querySelectorAll(".choice")
+const answerOneText = document.getElementById("answerOneText")
+const answerTwoText = document.getElementById("answerTwoText")
+const answerThreeText = document.getElementById("answerThreeText")
+const submit = document.getElementById("submit-button")
+

e1cbed91159fd791385bc6ae121d4c8d4fc8dcbb
sobitd <sobitdaniel@gmail.com>
2022-05-15 16:40:37 -0400

    add array object that holds the questions and answers

--- Assets/index.js
+++ Assets/index.js
@@ -7,3 +7,26 @@ const answerTwoText = document.getElementById("answerTwoText")
 const answerThreeText = document.getElementById("answerThreeText")
 const submit = document.getElementById("submit-button")
 
+//setting up the questions array where DOM will be used to populate the HTML elements
+var questionsArray = [
+    {
+        question: "What is the shortform of HTML?",
+        answerOne: "HTML",
+        answerTwo: "HMLT",
+        answerThree: "MLHT",
+        correct: "answerOne",
+    },
+    {
+        question: "What is the other name for Javascript?",
+        answerOne: "ECMAscript",
+        answerTwo: "JS",
+        answerThree: "I don't know",
+        correct: "answerOne",
+    },
+    {
+        question: "Is C++ a low level language?",
+        answerOne: "Yes",
+        answerTwo: "No",
+        correct: "answerOne",
+    },
+];

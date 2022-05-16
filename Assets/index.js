6737505db8f5aa3517bdedfd224ead4a78fec486
sobitd <sobitdaniel@gmail.com>
2022-05-16 17:22:11 -0400

    Modify html css and javascript files

--- Assets/index.js
+++ Assets/index.js
@@ -1,15 +1,26 @@
-//Declaring variables for DOM manipulation
+//Selecting elements for DOM manipulation
 const userAnswers = document.getElementById("userForm")
-const question =  document.getElementById("question")
+
+const question = document.getElementById("question")
+const questionOne = document.getElementById("questionOne")
+const questionTwo = document.getElementById("questionTwo")
+
 const choices = document.querySelectorAll(".choice")
 const answerOneText = document.getElementById("answerOneText")
 const answerTwoText = document.getElementById("answerTwoText")
 const answerThreeText = document.getElementById("answerThreeText")
+const answerFourText = document.getElementById("answerFourText")
+const answerFiveText = document.getElementById("answerFiveText")
+const answerSixText = document.getElementById("answerSixText")
+const answerSevenText = document.getElementById("answerSevenText")
+const answerEightText = document.getElementById("answerEightText")
+const correct = ""
+const currentQuestion = 0
 const submit = document.getElementById("submit-button")
+const restart = document.getElementById("restartQuiz")
 
 //setting up the questions array where DOM will be used to populate the HTML elements
-var questionsArray = [
-    {
+var questionsArray = [{
         question: "What is the shortform of HTML?",
         answerOne: "HTML",
         answerTwo: "HMLT",
@@ -17,49 +28,72 @@ var questionsArray = [
         correct: "answerOne",
     },
     {
-        question: "What is the other name for Javascript?",
-        answerOne: "ECMAscript",
-        answerTwo: "JS",
-        answerThree: "I don't know",
-        correct: "answerOne",
+        questionOne: "What is the other name for Javascript?",
+        answerFour: "ECMAscript",
+        answerFive: "JS",
+        answerSix: "I don't know",
+        correct: "answerFour",
     },
     {
-        question: "Is C++ a low level language?",
-        answerOne: "Yes",
-        answerTwo: "No",
-        correct: "answerOne",
+        questionTwo: "Is C++ a low level language?",
+        answerSeven: "Yes",
+        answerEight: "No",
+        correct: "answerSeven",
     },
 ];
 
 //Function to populate the html form with questions and answers that a user chooses
 
-var lastQuizPosition = questionsArray.length - 1; //index starts from 0 so the - 1 is to find the last index in an array
-var currentQuizPosition = 0; //the currect question position in the array
+//the currect question position in the array
 
-function showQuiz(){ 
-    var qn = questionsArray[currentQuizPosition];
+function populateQuiz() {
+    var qn = questionsArray[0];
     question.innerHTML = qn.question;
     answerOneText.innerHTML = qn.answerOne;
     answerTwoText.innerHTML = qn.answerTwo;
     answerThreeText.innerHTML = qn.answerThree;
+
+
+    var qn = questionsArray[1];
+    questionOne.innerHTML = qn.questionOne;
+    answerFourText.innerHTML = qn.answerFour;
+    answerFiveText.innerHTML = qn.answerFive;
+    answerSixText.innerHTML = qn.answerSix;
+
+    var qn = questionsArray[2];
+    questionTwo.innerHTML = qn.questionTwo;
+    answerSevenText.innerHTML = qn.answerSeven;
+    answerEightText.innerHTML = qn.answerEight;
+
+    if(choices.correct){
+
+    }
 }
 
-showQuiz();
+populateQuiz();
+
+//This displays results on the results page after user clicks submit
+
+submit.addEventListener('click', displayResults);
+
+function displayResults(){
+    window.open('Assets/results.html', '_self');
+
+    return displayResults;
+};
+
 
 //function to check on the correctness of user's choice
 
-function unselectedChoice (){
+function unselectedChoice() {
     choices.forEach(choices => choices.checked = false)
 }
 
-function selectedChoice(){
+function selectedChoice() {
     choices.forEach(choice => {
-        if(choice.checked){
+        if (choice.checked) {
             choice = choice.id
         }
     })
-    return choice
+    return choices
 }
-
-//when the submit button is clicked 
-submit.addEventListener('click', )
